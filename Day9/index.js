5011cafec7ca9daf758416937d67604187567d6f
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2025-02-22 12:33:25 +0530

    JavaScript

---
+++ Day9/index.js
@@ -0,0 +1,50 @@
+// class Me {
+//     name = "Jirayu"; // default public 
+//     #quantity = 20;        //# = private  
+//     address = "Bangkok";
+
+//     greet() {
+//         console.log("Hello, my name is " + this.name);
+//     }
+//     feed() {
+//         console.log("I'm eating "+this.#quantity + "burgers");
+//     }
+// }
+
+// let obj = new Me();
+// console.log(obj.name);
+// obj.feed();
+// // console.log(obj.#quantity); // it will give error because #age is private 
+
+
+// this is  parametrised constructor 
+// class Me {
+//     name;
+//     address;
+
+//    constructor (name, address){
+//          this.name = name;
+//          this.address = address;
+//    }
+// }
+// let obj = new Me("Piyush", 20);
+// console.log(obj.name);
+// console.log(obj.address); // it will give error because #age is private
+
+
+
+// this is parameterised constructor
+// class Me {
+//     name = "sadfh";
+//     address= "fljsd";
+
+//    constructor (){
+//         console.log("This is parameterised Constructor")
+//    }
+// }
+// let obj = new Me();
+// console.log(obj.name);
+// console.log(obj.address); // it will give error because #age is private
+
+
+
